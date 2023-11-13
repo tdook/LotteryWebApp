49b0aa11f808387fbd68459f07885c645c15c475
tdook <c2020110@ncl.ac.uk>
2023-11-13 14:09:57 +0000

    starting registration validation, email done, fname lname starting to be implemented - 500 error

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import InputRequired, Email, ValidationError
 
+
+def fname_lname_check (form,field):
+    excluded_chars = "*?!'^+%&/()=}][{$#@<>"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(f"Character {char} is nor allowed")
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
+    email = EmailField(validators=[InputRequired(),Email('Please enter a valid email address')])
+    firstname = StringField(validators=[fname_lname_check])
+    lastname = StringField(validators=[fname_lname_check])
     phone = StringField()
     password = PasswordField()
     confirm_password = PasswordField()
